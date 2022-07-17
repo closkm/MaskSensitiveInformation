3629c81530bc294b90ab87a12e4fa8a886faad09
Michael Closky <mikeclosky29@gmail.com>
2022-07-17 18:15:45 -0400

    Add project files.

---
+++ MaskSensitiveInformation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace MaskSensitiveInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a secret");
+
+            string secret = Console.ReadLine();
+
+            if(secret.Length <= 4)
+            {
+                Console.WriteLine("choose a longer secret next time");
+                StringBuilder newSecret = new StringBuilder();
+                for (int i = 0; i < secret.Length; i++)
+                {
+                    newSecret.Append(secret[i]);
+                }
+                Console.WriteLine(newSecret);
+            }else
+            {
+                StringBuilder newSecret = new StringBuilder();
+                for (int i = 0; i < secret.Length; i++)
+                {
+                    if(i < secret.Length - 4)
+                    {
+                    newSecret.Append("*");
+                    }
+                    else
+                    {
+                        newSecret.Append(secret[i]);
+                    }
+                }
+                Console.WriteLine(newSecret);
+            }
+        }
+    }
+}
